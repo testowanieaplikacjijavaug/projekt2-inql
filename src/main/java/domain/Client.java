5b2237f333954e9938c26ab7747592049714ac10
inql <dbinkus96@gmail.com>
2019-04-23 22:06:48 +0200

    Add Client model implementation

--- src/main/java/domain/Client.java
+++ src/main/java/domain/Client.java
@@ -1,4 +1,33 @@
 package domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String name;
+
+    @Column(unique = true)
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String surname;
+
+    @Column(unique = true)
+    @NotNull
+    @Size(min = 2, max =30)
+    private String email;
 }
