314fdea4766e1293e22a93c4f800634c4f6efd31
inql <dbinkus96@gmail.com>
2019-05-05 23:44:26 +0200

    Add implementation for order service

--- src/main/java/org/inql/onlineshop/repository/OrderRepository.java
+++ src/main/java/org/inql/onlineshop/repository/OrderRepository.java
@@ -1,11 +1,9 @@
 package org.inql.onlineshop.repository;
 
-import org.inql.onlineshop.domain.Client;
 import org.inql.onlineshop.domain.Item;
 import org.inql.onlineshop.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Map;
 import java.util.Set;
 
 public interface OrderRepository extends CrudRepository<Order,Long> {
